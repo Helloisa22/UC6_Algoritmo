12ef01ff0d5a9db7bea112afd8db9d978b62017b
Helloisa22 <helloisa.betriz44@gmail.com>
2026-03-06 22:08:12 -0300

    Aula 11 nao finalizada

---
+++ Aula11/aula11.py
@@ -0,0 +1,50 @@
+import random
+import math
+import datetime
+
+# numero_aleatorio = random.randint(1000, 2000)
+# # RANDINT > Gera apenas numeros aleatorios
+# print(numero_aleatorio)
+
+# Sorteio de aluno
+# alunos = ["Israel", "Adenilson", "Anna", "Wellington", "Jonathan", "Isabelly", "João Pedro", "Luis Felipe", "Iara", "Vanessa", "Daniel", "João Paulo", "Lucas", "Bernardo", "Camila", "Stefany", "Guilherme", "Micael", "Kauan"]
+# sorteado = random.choice(alunos)
+# sorteado2 = random.choice(alunos)
+# # CHOICE escolher de forma aleatoria
+
+# print("Dupla Dinamica", sorteado, " - " , sorteado2)
+
+
+# SQRT = raiz quadrada
+# numero = 13890
+# raiz = math.sqrt(numero)
+# print(raiz)
+
+# # Elevar um numero
+# print(math.pow(2, 2))
+
+
+# TRABALHANDO COM DATAS
+# agora = datetime.datetime.now()
+
+# print(agora.hour)
+
+
+
+
+# Exercicio
+# Solicitar ao usuario um numero de 1 até 5
+# Gerar um numero aleatorio usando a biblioteca random.randint
+# Verificar se o usuario digitou o mesmo valor que o resultado da funcao randint
+# numero_usuario = int(input("Digite um numero da sorte de 1 até 5: "))
+# numero_sorte = random.randint(1, 7000)
+# if numero_usuario == numero_sorte:
+#     print("Parabén Fulano, você ganhou um aperto de mão e um dois reais")
+# else:
+#     print("Errou, tente novamente")
+
+
+
+
+
+
