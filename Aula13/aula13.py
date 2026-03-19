7ef082fabf119cc0093b02df09180c36c471a418
Helloisa22 <helloisa.betriz44@gmail.com>
2026-03-18 22:10:34 -0300

    Aula 13

---
+++ Aula13/aula13.py
@@ -0,0 +1,21 @@
+import pymysql as pySQL
+
+# CONEXAO COM O BANCO DE DADOS
+conexao = pySQL.connect(
+    host="localhost",         # endereço do servidor (local = "localhost")
+    user="root",              # usuário do MySQL
+    password="",              # senha do MySQL
+    database="bd_livrariaonline",      # banco que você já criou
+    port=3306                 # porta padrão do MySQL (opcional)
+)
+
+# Cria o cursor — versão dicionário (retorna {"coluna": valor})
+cursor = conexao.cursor(pySQL.cursors.DictCursor)
+
+
+# ── Buscar todos os registros ───────────────
+cursor.execute("SELECT * FROM clientes")
+todos = cursor.fetchall()
+
+for cliente in todos:
+    print(cliente["nome"],"-", cliente["email"],"-", cliente["telefone"])
